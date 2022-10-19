658ca1ca4b4595846f7f7112d332c32bc76917f7
dParikesit <dsparikesit@gmail.com>
2022-10-19 14:29:47 +0700

    feat: add service setup

---
+++ src/App/Service/Service.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+if (!defined('BASEPATH')){
+  require_once "../../inc/config.php";
+}
+
+use PDO;
+
+final class Service
+{
+    protected $db;
+
+    private $DB_OPTIONS = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
+
+    /**
+     * Make a connection to the resource.
+     */
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO("pgsql:host={$_ENV['POSTGRES_HOST']};dbname={$_ENV['POSTGRES_DB']}", $_ENV['POSTGRES_USER'], $_ENV['POSTGRES_PASSWORD'], $this->DB_OPTIONS);
+            return "Successfully Connected";
+        } catch (\PDOException $e) {
+            die("Error ! " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Close connection to the resource.
+     */
+    public function __destruct()
+    {
+    $this->db = null;
+    }
+}
+
+?>
---
+++ src/App/Service/Users.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Service;
+
+class UsersService extends Service{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function create(string $email, string $hashed_pass, string $username) {
+        try {
+            $sql = "INSERT INTO users (email, password, username) VALUES (:email, :password, :username)";
+
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':email', $email, PDO::PARAM_STR);
+            $statement->bindParam(':password', $hashed_pass, PDO::PARAM_STR);
+            $statement->bindParam(':username', $username, PDO::PARAM_STR);
+            $statement->execute();
+
+            return array("Status code" => 201,"Message"=>"Successfully Created");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+}
+
+?>
