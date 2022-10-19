a38aa01cd72aa797bf9f591a5b0dc064ce652080
Zeus-s <kristabdixx@gmail.com>
2022-10-19 20:54:00 +0700

    add album and song service

---
+++ src/App/Service/AlbumService.php
@@ -0,0 +1,100 @@
+<?php
+namespace App\Service;
+use \PDO;
+
+class AlbumService extends Service{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function create(string $judul, string $penyanyi, int $total_duration, string $image_path, string $tanggal_terbit, string $genre) {
+        try {
+            $sql = "INSERT INTO albums (judul, penyanyi, total_duration, image_path, tanggal_terbit, genre) VALUES (:judul, :penyanyi, :total_duration, :image_path, :tanggal_terbit, :genre)";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':judul', $judul, PDO::PARAM_STR);
+            $statement->bindParam(':penyanyi', $penyanyi, PDO::PARAM_STR);
+            $statement->bindParam(':total_duration', $total_duration, PDO::PARAM_INT);
+            $statement->bindParam(':image_path', $image_path, PDO::PARAM_STR);
+            $statement->bindParam(':tanggal_terbit', $tanggal_terbit, PDO::PARAM_STR);
+            $statement->bindParam(':genre', $genre, PDO::PARAM_STR);
+            $statement->execute();
+
+            return array("Status code" => 201,"Message"=>"Successfully Created");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function delete($album_id) {
+        try {
+            $sql = "DELETE FROM albums WHERE album_id = :album_id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':album_id', $album_id, PDO::PARAM_INT);
+            $statement->execute();
+
+            $sql = "UPDATE albums SET album_id = NULL WHERE album_id = :album_id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':album_id', $album_id, PDO::PARAM_INT);
+            $statement->execute();
+
+            return array("Status code" => 200,"Message"=>"Successfully Deleted");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function update($album_id, string $judul, string $penyanyi, string $image_path, string $tanggal_terbit, string $genre) {
+        try {
+            $sql = "UPDATE albums SET judul = :judul, penyanyi = :penyanyi, image_path = :image_path, tanggal_terbit = :tanggal_terbit, genre = :genre WHERE album_id = :album_id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':album_id', $album_id, PDO::PARAM_INT);
+            $statement->bindParam(':judul', $judul, PDO::PARAM_STR);
+            $statement->bindParam(':penyanyi', $penyanyi, PDO::PARAM_STR);
+            $statement->bindParam(':image_path', $image_path, PDO::PARAM_STR);
+            $statement->bindParam(':tanggal_terbit', $tanggal_terbit, PDO::PARAM_STR);
+            $statement->bindParam(':genre', $genre, PDO::PARAM_STR);
+            $statement->execute();
+
+            return array("Status code" => 200,"Message"=>"Successfully Updated");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function readAll() {
+        try {
+            $sql = "SELECT * FROM albums ORDER BY judul ASC";
+            $statement = $this->db->prepare($sql);
+            $statement->execute();
+            $result = $statement->fetchAll();
+
+            return array("Status code" => 200,"Message"=>"Successfully Read","Data"=>$result);
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function getAlbumById($album_id) {
+        try {
+            $sql = "SELECT * FROM albums WHERE album_id = :album_id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':album_id', $album_id, PDO::PARAM_INT);
+            $statement->execute();
+            $result = $statement->fetch();
+
+            $sql = "SELECT * FROM songs WHERE album_id = :album_id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':album_id', $album_id, PDO::PARAM_INT);
+            $statement->execute();
+            $result2 = $statement->fetch();
+
+            return array("Status code" => 200,"Message"=>"Successfully Read","Data"=>$result,"Songs"=>$result2);
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+}
+
+?>
---
+++ src/App/Service/SongService.php
@@ -0,0 +1,103 @@
+<?php
+namespace App\Service;
+use \PDO;
+
+class SongService extends Service{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function create(string $judul, string $penyanyi, string $tanggal_terbit, string $genre, string $duration, string $audio_path, string $image_path) {
+        try {
+            $sql = "INSERT INTO songs (judul, penyanyi, tanggal_terbit, genre, duration, audio_path, image_path) VALUES (:judul, :penyanyi, :tanggal_terbit, :genre, :duration, :audio_path, :image_path)";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':judul', $judul, PDO::PARAM_STR);
+            $statement->bindParam(':penyanyi', $penyanyi, PDO::PARAM_STR);
+            $statement->bindParam(':tanggal_terbit', $tanggal_terbit, PDO::PARAM_STR);
+            $statement->bindParam(':genre', $genre, PDO::PARAM_STR);
+            $statement->bindParam(':duration', $duration, PDO::PARAM_INT);
+            $statement->bindParam(':audio_path', $audio_path, PDO::PARAM_STR);
+            $statement->bindParam(':image_path', $image_path, PDO::PARAM_STR);
+            $statement->execute();
+
+            return array("Status code" => 201,"Message"=>"Successfully Created");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function delete($id) {
+        try {
+            $sql = "DELETE FROM songs WHERE id = :id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+
+            return array("Status code" => 201,"Message"=>"Successfully Deleted");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function update($id, string $judul, string $penyanyi, string $tanggal_terbit, string $genre, string $audio_path, string $image_path) {
+        try {
+            $sql = "UPDATE songs SET judul=:judul, penyanyi=:penyanyi, tanggal_terbit=:tanggal_terbit, genre=:genre, audio_path=:audio_path, image_path=:image_path WHERE id = :id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->bindParam(':judul', $judul, PDO::PARAM_STR);
+            $statement->bindParam(':penyanyi', $penyanyi, PDO::PARAM_STR);
+            $statement->bindParam(':tanggal_terbit', $tanggal_terbit, PDO::PARAM_STR);
+            $statement->bindParam(':genre', $genre, PDO::PARAM_STR);
+            $statement->bindParam(':audio_path', $audio_path, PDO::PARAM_STR);
+            $statement->bindParam(':image_path', $image_path, PDO::PARAM_STR);
+            $statement->execute();
+
+            return array("Status code" => 201,"Message"=>"Successfully Updated");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function updateSongToAlbum($id, $album_id) {
+        try {
+            $sql = "UPDATE songs SET album_id=:album_id WHERE id = :id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->bindParam(':album_id', $album_id, PDO::PARAM_INT);
+            $statement->execute();
+
+            return array("Status code" => 201,"Message"=>"Successfully Updated Song to Album");
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function readAll() {
+        try {
+            $sql = "SELECT * FROM songs ORDER BY jUdul asc";
+            $statement = $this->db->prepare($sql);
+            $statement->execute();
+            $result = $statement->fetchAll();
+
+            return array("Status code" => 200,"Message"=>"Successfully Read","Data"=>$result);
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+
+    public function getSongById($id) {
+        try {
+            $sql = "SELECT * FROM songs WHERE id = :id";
+            $statement = $this->db->prepare($sql);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+            $result = $statement->fetch();
+
+            return array("Status code" => 200,"Message"=>"Successfully Read","Data"=>$result);
+        } catch (PDOException $e) {
+            return array("Status code" => 400,"Message"=>"Error: [all] {$e->getMessage()}");
+        }
+    }
+}
+
+?>
