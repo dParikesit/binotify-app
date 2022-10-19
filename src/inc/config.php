740ba0b3be66f0f017038635276da3cc24d14f5e
dParikesit <dsparikesit@gmail.com>
2022-10-19 14:30:09 +0700

    feat: add config

---
+++ src/inc/config.php
@@ -0,0 +1,25 @@
+<?php
+
+error_reporting(E_ALL);
+
+session_start();
+
+define("ROOT", dirname(__DIR__).DIRECTORY_SEPARATOR);
+
+define("BASEPATH", TRUE);
+
+define("URL", "http://localhost:3000");
+
+spl_autoload_register(function ($className) 
+{
+    $fileName = sprintf("%s%s.php", ROOT, str_replace("\\", "/", $className));
+
+    if (file_exists($fileName)) 
+    {
+        require ($fileName);
+    } 
+    else 
+    {
+        echo "file not found {$fileName}";
+    }
+});
