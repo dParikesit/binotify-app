c9ddd0d420644962a183e09949ed9cadb27dcb3a
dParikesit <dsparikesit@gmail.com>
2022-10-21 14:56:34 +0700

    fix: modify schema

--- database/migration/3-songs-table.sql
+++ database/migration/3-songs-table.sql
@@ -1,15 +1,3 @@
-/* CREATE TABLE IF NOT EXISTS songs (
-    song_id INTEGER primary key,
-    Judul VARCHAR(64) NOT NULL,
-    Penyanyi VARCHAR(128) NOT NULL,
-    Tanggal_terbit date NOT NULL,
-    Genre VARCHAR(64) NOT NULL,
-    Duration INTEGER NOT NULL,
-    Audio_path VARCHAR(256) NOT NULL,
-    Image_path VARCHAR(256) NOT NULL,
-    CONSTRAINT album_id_fk FOREIGN KEY(album_id) REFERENCES albums(album_id)
-); */
-
 CREATE TABLE IF NOT EXISTS songs (
     song_id INTEGER primary key,
     Judul VARCHAR(64) NOT NULL,
@@ -19,8 +7,8 @@ CREATE TABLE IF NOT EXISTS songs (
     Duration INTEGER NOT NULL,
     Audio_path VARCHAR(256) NOT NULL,
     Image_path VARCHAR(256) NOT NULL,
-    album_id INTEGER REFERENCES albums (album_id)
+    album_id INTEGER REFERENCES albums (album_id) ON UPDATE CASCADE on DELETE CASCADE
 );
 
-ALTER TABLE songs
-ADD CONSTRAINT fk_album_id FOREIGN KEY (album_id) REFERENCES albums (album_id);
+-- ALTER TABLE songs
+-- ADD CONSTRAINT fk_album_id FOREIGN KEY (album_id) REFERENCES albums (album_id);
