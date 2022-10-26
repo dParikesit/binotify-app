d7a914b70ca9959a301c8755426a0d1483855e5e
Zeus-s <kristabdixx@gmail.com>
2022-10-26 21:27:56 +0700

    fix styling add album

--- src/App/View/addalbum.php
+++ src/App/View/addalbum.php
@@ -8,55 +8,61 @@
     }
 ?>
 
+<?php include 'navbar.php'; require_once "inc/config.php";?>
+<?php include 'sidebar.php';?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="./layout/assets/css/add.css" />
+        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/addpage.css" />
+        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/navbar.css">
         <title>Binotify</title>
     </head>
 
-    <body>      
-        <div id="main">
-            <div class="container">
-                <p class="title">Add Album</p>
-                <p id="error"></p>
-                <div class="content">
-                    <div class="content-left">
-                        <!-- form for editing field, upload image and audio file -->
-                        <form class="form_edit" id="form_edit" method="PUT" enctype="multipart/form-data">
-                            <label>
-                                Title : 
-                                <input class="input" type="text" name="judul" id="judul" placeholder="Title" required>
-                            </label>
-                            <br>
-                            <label>
-                                Artist :
-                                <input class="input" type="text" name="penyanyi" id="penyanyi" placeholder="Artist" required>
-                            </label>
-                            <br>
-                            <label >
-                                Cover Album : 
-                                <input class="button" type="file" name="cover_file" id="cover_file" accept="image/jpg, image/jpeg, image/png" required>
-                            </label>
-                            <br>
-                            <label>
-                                Genre :
-                                <input class="input" type="text" name="genre" id="genre" placeholder="Genre" required>
-                            </label>
-                            <br>
-                            <label>
-                                Tanggal Terbit :
-                                <input type="date" id="tanggal_terbit" name="tanggal_terbit" value="2022-10-28"  min="1960-01-01" required>
-                            </label>
-                        </form>
-                        <button type="submit" id="button_submit" class="button" onclick={add(event)}>
-                            Submit
-                        </button>
-                    </div>
-                </div>
+    <body>
+        <?php
+            $isAdmin = true;
+            navbar(true, "USERNAME");
+        ?>      
+        <div class="flex">
+            <?php
+                sidebar(true);
+            ?>   
+            <div class="content">
+                <h1 class="title">Add Album</h1>
+                <form class="form_edit" id="form_edit" method="PUT" enctype="multipart/form-data">
+                    <label>
+                        Title : 
+                        <input class="input" type="text" name="judul" id="judul" placeholder="Title" required>
+                    </label>
+                    <br>
+                    <label>
+                        Artist :
+                        <input class="input" type="text" name="penyanyi" id="penyanyi" placeholder="Artist" required>
+                    </label>
+                    <br>
+                    <label >
+                        Cover Album : 
+                        <input class="button" type="file" name="cover_file" id="cover_file" accept="image/jpg, image/jpeg, image/png" required>
+                    </label>
+                    <br>
+                    <label>
+                        Genre :
+                        <input class="input" type="text" name="genre" id="genre" placeholder="Genre" required>
+                    </label>
+                    <br>
+                    <label>
+                        Tanggal Terbit :
+                        <input type="date" id="tanggal_terbit" name="tanggal_terbit" value="2022-10-28"  min="1960-01-01" required>
+                    </label>
+                </form>
+                <br>
+                <button type="submit" id="button_submit" class="button" onclick={add(event)}>
+                    Submit
+                </button>
             </div>
         </div>
     </body>
@@ -87,6 +93,8 @@
 
             const xmlhttp = new XMLHttpRequest();
             xmlhttp.open("POST", "/addalbum");
+            // xmlhttp.setRequestHeader("Content-type","multipart/form-data; charset=utf-8; boundary=" + Math.random().toString().substr(2));
+            // xmlhttp.send(JSON.stringify(payload));
             xmlhttp.send(formData);
         }
     </script>
