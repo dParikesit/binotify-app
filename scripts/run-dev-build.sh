6d841e4d195f9c4012f9f57c72b9981b032be051
dParikesit <dsparikesit@gmail.com>
2022-10-26 20:37:56 +0700

    feat: add uploads image

--- scripts/run-dev-build.sh
+++ scripts/run-dev-build.sh
@@ -1,2 +1,3 @@
+chmod -R 777 src/uploads
 docker compose -p "tubes-1" -f ./docker/docker-compose.dev.yml --env-file .env down -v
 docker compose -p "tubes-1" -f ./docker/docker-compose.dev.yml --env-file .env up --build
--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,1 +1,2 @@
+chmod -R 777 src/uploads
 docker compose -p "tubes-1" -f ./docker/docker-compose.dev.yml --env-file .env up
