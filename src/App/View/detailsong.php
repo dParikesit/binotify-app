9313b2457a272f21ba19fee57f060b9a7c810345
pandora-1 <michaelkahari.mk@gmail.com>
2022-10-28 02:14:18 +0700

    finishing styling in detail song

--- src/App/View/detailsong.php
+++ src/App/View/detailsong.php
@@ -14,7 +14,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/detailalbum.css" />
+        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/detailsong.css" />
         <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/nav.css">
         <link rel="icon" type="image/x-icon" href="<?php echo URL; ?>/layout/assets/img/favicon.png">
         <title>Binotify</title>
@@ -34,52 +34,24 @@
             <div class="container">
                 <div class="content">
                     <div class="user_content">
-                        <img src="https://i.scdn.co/image/ab67616d0000b2739abdf14e6058bd3903686148" alt="cover" id="cover" class="cover">
-                        <div class="content-left">
-                            <a class="judul" id="judul">Title: </a>
-                            <div class="desc">
-                            <a class="penyanyi" id="penyanyi">Artist: </a>
-                            <a class="tanggal_terbit" id="tanggal_terbit">Release: </a><br>
-                            </div>
-                            <div class="desc">
-                            <a class="genre" id="genre">Genre: </a>
-                            <a class="duration" id="duration">Duration: </a><br>
-                            </div>
-                            <!-- Button to see Album -->
-                            <button type="button" id="button_album" class="button-album">
-                                See Album
-                            </button>
-                            <!-- Audio -->
-                            <audio class="audio_source" id="audio" controls>
-                                <source src="" type="" id="audio_source">
-                            </audio>
+                        <img src="https://i.scdn.co/image/ab67616d0000b2739abdf14e6058bd3903686148" alt="cover" id="cover" class="cover"><br>
+                        <a class="judul" id="judul">Title: </a>
+                        <div class="desc">
+                        <a class="penyanyi" id="penyanyi">Artist: </a>
+                        <a class="tanggal_terbit" id="tanggal_terbit">Release: </a>
+                        <a class="genre" id="genre">Genre: </a>
                         </div>
-                    </div>
-                    <div class="admin_content" id="admin_content">
-                        <form class="form_edit" id="form_edit" method="PUT" enctype="multipart/form-data">
-                        <section>
-                            <label> Title: </label>
-                            <input type="text" name="judul" id="judul_edit" placeholder="Title"><br>
-                        </section>
-                        <section>
-                            <label> Tanggal Terbit: </label>
-                            <input type="date" name="tanggal_terbit" id="tanggal_terbit_edit"  value="2022-10-28"  min="1960-01-01"><br>
-                        </section>
-                        <section>
-                            <label> Genre: </label>
-                            <input type="text" name="genre" id="genre_edit" placeholder="Genre"><br>
-                        </section>
-                            <label> Audio File: </label>
-                            <input type="file" name="audio_file" id="audio_file_edit" accept="audio/mp3, audio/wav, audio/ogg"><br>
-                            <label> Cover Image: </label>
-                            <input type="file" name="cover_file" id="cover_file_edit" accept="image/jpg, image/jpeg, image/png"><br>
-                        </form>
-                        <button type="button" id="button_submit" class="button" onclick={updateSong(event)}>
-                            Submit
+                        <div class="desc">
+                        <a class="duration" id="duration">Duration: </a>
+                        </div>
+                        <!-- Button to see Album -->
+                        <button type="button" id="button_album" class="button-album">
+                            See Album
                         </button>
-                        <button type="button" id="button_delete" class="button" onclick={deleteSong(event)}>
-                            Delete
-                        </button>    
+                        <!-- Audio -->
+                        <audio class="audio_source" id="audio" controls>
+                            <source src="" type="" id="audio_source">
+                        </audio>
                     </div>
                     <?php 
                     if (!isset($_SESSION["user_id"]) || (isset($_SESSION["user_id"]) && !$_SESSION["isAdmin"])) { ?>
@@ -88,12 +60,36 @@
                         </script>
                     <?php } ?>
                 </div>
+                <div class="admin_content" id="admin_content">
+                    <div class="edit-form">
+                        <h1 class="edit-form-title">Edit song</h1>
+                        <form class="form_edit_bottom" id="form_edit" method="PUT" enctype="multipart/form-data">
+                        <section>
+                            <label> Title: </label>
+                            <input class="form_edit_bottom_input" type="text" name="judul" id="judul_edit" placeholder="Title"><br>
+                        </section>
+                        <section>
+                            <label> Tanggal Terbit: </label>
+                            <input class="form_edit_bottom_input" type="date" name="tanggal_terbit" id="tanggal_terbit_edit"  value="2022-10-28"  min="1960-01-01"><br>
+                        </section>
+                        <section>
+                            <label> Genre: </label>
+                            <input class="form_edit_bottom_input" type="text" name="genre" id="genre_edit" placeholder="Genre"><br>
+                        </section>
+                            <label> Audio File: </label>
+                            <input class="form_edit_bottom_input" type="file" name="audio_file" id="audio_file_edit" accept="audio/mp3, audio/wav, audio/ogg"><br>
+                            <label> Cover Image: </label>
+                            <input class="form_edit_bottom_input" type="file" name="cover_file" id="cover_file_edit" accept="image/jpg, image/jpeg, image/png"><br>
+                        </form>
+                        <button type="button" id="button_submit" class="button_save" onclick={updateSong(event)}>
+                            Submit
+                        </button>
+                        <button type="button" id="button_delete" class="button_delete" onclick={deleteSong(event)}>
+                            Delete
+                        </button>    
+                    </div>
+                </div>
             </div>
-<!--             <div class="play-song">
-           Audio
-            <audio class="audio_source" controls>
-                <source src="" type="" id="audio">
-            </audio> -->
         </div>
         </div>
         
@@ -115,9 +111,9 @@
             image.setAttribute('src', `/images?name=${data.image_path}`);
 
             document.getElementById('judul').innerHTML = `${data.judul}`;
-            document.getElementById('penyanyi').innerHTML = `${data.penyanyi},`;
-            document.getElementById('tanggal_terbit').innerHTML = `${data.tanggal_terbit}`;
-            document.getElementById('genre').innerHTML = `${data.genre},`;
+            document.getElementById('penyanyi').innerHTML = `${data.penyanyi} .`;
+            document.getElementById('tanggal_terbit').innerHTML = `${data.tanggal_terbit} .`;
+            document.getElementById('genre').innerHTML = `${data.genre}`;
             document.getElementById('duration').innerHTML = `${Math.floor(data.duration/60)}m ${data.duration%60}s`;
             document.getElementById('judul_edit').value = data.judul;
             document.getElementById('tanggal_terbit_edit').value = data.tanggal_terbit;
