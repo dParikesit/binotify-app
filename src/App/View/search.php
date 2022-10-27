c4322618209d59e602d7e7e92267749c202503a9
pandora-1 <michaelkahari.mk@gmail.com>
2022-10-27 09:38:27 +0700

    finishing styling in search page

--- src/App/View/search.php
+++ src/App/View/search.php
@@ -13,7 +13,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/search-2.css">
+        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/search.css">
         <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/nav.css">
         <title>Binotify</title>
     </head>
@@ -51,7 +51,7 @@
                         type="genre"
                         name="genre"
                         id="genre"
-                        placeholder="What's on your mind?"
+                        placeholder="Mau genre apa?"
                         />
                         <label class="label-order" for="order">Order by:</label>
                         <select id="order" name="order" class="order">
@@ -97,7 +97,7 @@
                             echo $i + 1 + $page;
                             echo "</td>";
                             echo "<td>";
-                            echo "<div class='flex'>";
+                            echo "<div class='flex wrapping-content'>";
                             // echo "<img src='" . $data[7] .  "' width='50' alt='song' />";
                             echo "<img src='https://i.scdn.co/image/ab67616d0000b2739abdf14e6058bd3903686148' width='50' alt='song' />";
                             echo "<div class='main-content'>";
@@ -116,7 +116,7 @@
                         }
                     ?>
                 </table>
-                <div class="flex">
+                <div class="flex floatright">
                 <form class="leftbutton" method="get" action="/search">
                         <label class="hide" for="page">Search</label>
                         <input
@@ -167,15 +167,17 @@
                         }
                         ?>"
                         />
-                        <input type="submit">
+                        <input type="submit" value="<" class="before">
                     </form>
                     <form class="rightbutton" method="get" action="/search">
                         <label class="hide" for="page">Search</label>
                         <input
                             class="hide" name="page"
                             value="<?php 
-                            if(isset($_GET["page"]) && $_GET["page"] != '') {
+                            if(isset($_GET["page"]) && $_GET["page"] != '' && $count_data == $maxdata) {
                                 echo $_GET["page"] + 1;
+                            } else if (isset($_GET["page"]) && $_GET["page"] != '' && $count_data < $maxdata) {
+                                echo $_GET["page"];
                             } else {
                                 echo 0;
                             }
@@ -215,7 +217,7 @@
                         }
                         ?>"
                         />
-                        <input type="submit">
+                        <input type="submit" value=">" class="after">
                     </form>
                 </div>
             </div>
