1b1019ba79b6409026d0110b5091654def82e7c0
pandora-1 <michaelkahari.mk@gmail.com>
2022-11-26 20:38:17 +0700

    add listpenyanyi premium page

--- src/App/View/listpenyanyi.php
+++ src/App/View/listpenyanyi.php
@@ -10,7 +10,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/homepage.css">
+        <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/listpenyanyi.css">
         <link rel="stylesheet" href="<?php echo URL; ?>/layout/assets/css/nav.css">
         <link rel="icon" type="image/x-icon" href="<?php echo URL; ?>/layout/assets/img/favicon.png">
         <title>Binotify</title>
@@ -30,34 +30,27 @@
             <table class="card">
             <tr>
                 <th class="first-index">#</th>
-                <th>Judul</th>
-                <th>Genre</th>
-                <th>Tahun</th>
+                <th>Nama Penyanyi</th>
+                <th></th>
             </tr>
             <?php
                 $songs = new App\Service\SongService();
                 $count_data = count($songs->getSong());
                 for($i = 0; $i < $count_data; $i++) {
                     $data = $songs->getSong()[$i];
-                    echo "<tr class='subcard' onClick={navigateTo('" . $data[0] .  "')}>";
+                    echo "<tr class='subcard'>";
                     echo "<td class='index'>";
                     echo $i + 1;
                     echo "</td>";
                     echo "<td>";
-                    echo "<div class='flex content-table'>";
-                    // echo "<img src='" . $data[7] .  "' width='50' alt='song' />";
-                    echo "<img src='"."/images?name=". $data["image_path"] . "' width='50' alt='song' />";
-                    echo "<div class='main-content'>";
                     echo "<div class='title'>" . $data[1] .  "</div>";
-                    echo "<div class='writer'>" . $data[2] . "</div>";
-                    echo "</div>";
-                    echo "</div>";
                     echo "</td>";
                     echo "<td>";
-                    echo "<div class='genre'>" . $data[4] .  "</div>";
-                    echo "</td>";
-                    echo "<td>";
-                    echo "<div class='year'>" . substr($data[3], 0, 4) . "</div>";
+                    if(true) { // IF subscribe then see song
+                        echo "<div class='button'>See Songs</div>";
+                    } else {
+                        echo "<div class='button'>Subscribe</div>";
+                    }
                     echo "</td>";
                     echo "</tr>";
                 }
